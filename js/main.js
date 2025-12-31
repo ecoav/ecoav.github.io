2f24fa728db6e4c54e0d27cc23dab85b9bd515bc
ecoav <ecordero@ies-eugeni.cat>
2025-12-31 02:39:55 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const faders = document.querySelectorAll('.fade');
+
+const appear = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+faders.forEach(el => appear.observe(el));
